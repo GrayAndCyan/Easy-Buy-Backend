3cc18e24223f7ac0a151e4a71bfc3de7ba8f4e58
mizore <2290551857@qq.com>
2024-04-10 14:37:43 +0800

    fix import error

--- src/test/java/com/mizore/easybuy/JWTTest.java
+++ src/test/java/com/mizore/easybuy/JWTTest.java
@@ -1,18 +1,14 @@
 package com.mizore.easybuy;
 
-import com.mizore.easybuy.utils.JWT;
-import io.jsonwebtoken.Jwts;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.crypto.SecretKey;
 
 @SpringBootTest
 public class JWTTest {
 
     @Test
     public void testSec() {
-        String secretKeyStr = JWT.getSecretKeyStr();
+//        String secretKeyStr = JWTUtil.getSecretKeyStr();
         // huHAQ21P5fsGgpnHKipREwz4zgwUxJ8bGl+gmW4mbl4=
         System.out.println();
     }
