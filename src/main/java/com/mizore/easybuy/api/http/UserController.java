e400d3a115863adf8046243af67d96764b7cf94b
ApprearingOnNullday <2543004661@qq.com>
2024-04-13 20:20:11 +0800

    userOpenStore-v2

--- src/main/java/com/mizore/easybuy/api/http/UserController.java
+++ src/main/java/com/mizore/easybuy/api/http/UserController.java
@@ -1,7 +1,12 @@
 package com.mizore.easybuy.api.http;
 
+import com.mizore.easybuy.model.vo.BaseVO;
+import com.mizore.easybuy.service.base.ITbUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,5 +20,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private ITbUserService tbUserService;
+
+    /**
+     * 用户“我要开店”
+     * @param name 店铺名称
+     * @param address 店铺地址
+     * @return
+     */
+    @PostMapping("openstore")
+    public BaseVO openStore(
+            @RequestParam(value = "name") String name,
+            @RequestParam(value = "address") String address
+    ){
+        return tbUserService.openStore(name, address);
+    }
 
 }
