8c6c3f12ca3ebdea67c4840cb5063d4da77b6da7
mizore <2290551857@qq.com>
2024-04-14 23:32:46 +0800

    fix renew token

--- src/main/java/com/mizore/easybuy/config/MvcConfig.java
+++ src/main/java/com/mizore/easybuy/config/MvcConfig.java
@@ -1,6 +1,8 @@
 package com.mizore.easybuy.config;
 
 import com.mizore.easybuy.interceptor.LoginInterceptor;
+import com.mizore.easybuy.service.base.ITbUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,9 +10,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+
+    @Autowired
+    private ITbUserService userService;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor())
+        registry.addInterceptor(new LoginInterceptor(userService))
                 .excludePathPatterns(
                         "/user/login",
                         "/user/sign"
--- src/main/java/com/mizore/easybuy/interceptor/LoginInterceptor.java
+++ src/main/java/com/mizore/easybuy/interceptor/LoginInterceptor.java
@@ -1,19 +1,34 @@
 package com.mizore.easybuy.interceptor;
 
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.google.common.collect.Maps;
 import com.mizore.easybuy.model.dto.UserDTO;
+import com.mizore.easybuy.model.entity.TbUser;
+import com.mizore.easybuy.model.vo.BaseVO;
+import com.mizore.easybuy.service.base.ITbUserService;
 import com.mizore.easybuy.utils.JWTUtil;
 import com.mizore.easybuy.utils.UserHolder;
 import io.jsonwebtoken.Claims;
+import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.io.IOException;
+import java.util.Map;
+
 @Slf4j
 public class LoginInterceptor implements HandlerInterceptor {
 
     private final static String TOKEN = "token";
+
+    private ITbUserService userService;
+
+    public LoginInterceptor(ITbUserService userService) {
+        this.userService = userService;
+    }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -26,14 +41,64 @@ public class LoginInterceptor implements HandlerInterceptor {
 
         // 解析token,保存用户对象上下文
         Claims claims = JWTUtil.parseJWT(token);
-        UserDTO loginUser = UserDTO.builder()
-                .id((Integer) claims.getOrDefault("id", 0))
-                .username((String) claims.getOrDefault("username", StrUtil.EMPTY))
-                .role((Integer) claims.getOrDefault("role", 1))
-                .build();
-        UserHolder.save(loginUser);
 
+        Integer id = claims.get("id", Integer.class);
+        String username = claims.get("username", String.class);
+        Integer role = claims.get("role", Integer.class);
+        if (id == null || username == null || role == null) {
+            log.error("未从token中解析得到需要的信息, token: {}, id: {}, username: {}, role: {}",
+                    token, id, username, role);
+            writeFailedMsg(response, "token错误。");
+            return false;
+        }
+
+        // 根据id去db查user，因为jwt-payload中的信息可能为脏数据，此时就需要换token
+        TbUser dbUser = userService.getById(id);
+        if (dbUser == null) {
+            log.error("未查询到 id = {} 用户", id);
+            writeFailedMsg(response, "token中id错误。");
+            return false;
+        }
+
+        String dbUsername = dbUser.getUsername();
+        Integer dbRole = dbUser.getRole();
+        UserDTO loginUser;
+        // db与token一致
+        if (username.equals(dbUsername) && role.equals(dbRole)) {
+            loginUser = UserDTO.builder()
+                    .id(id)
+                    .username(username)
+                    .role(role)
+                    .build();
+            UserHolder.save(loginUser);
+            return true;
+        }
+
+        // 不一致，重新生成
+        loginUser = UserDTO.builder()
+                .id(id)
+                .username(dbUsername)
+                .role(dbRole)
+                .build();
+        Map<String, Object> newClaims = Maps.newHashMap();
+        newClaims.put("id", id);
+        newClaims.put("username", dbUsername);
+        newClaims.put("role", dbRole);
+        String newToken = JWTUtil.generateJWT(newClaims);
+        response.setHeader(TOKEN, newToken);  // 前端需以此更新token
+        UserHolder.save(loginUser);
         return true;
+    }
+
+    // 在响应体中写指定失败msg
+    private void writeFailedMsg(HttpServletResponse response, String s) {
+        BaseVO<String> baseVO = new BaseVO<String>().failure();
+        baseVO.setData(s);
+        try (ServletOutputStream outputStream = response.getOutputStream()) {
+            outputStream.write(JSONUtil.toJsonStr(baseVO).getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
